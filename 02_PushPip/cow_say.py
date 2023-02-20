91f9d28deab52688ffb5372ec656ab8a3a5b7874
Чистобаев Андрей Андреевич <chistobaevaa@arccn.local>
2023-02-20 23:53:42 +0300

    Написана cow_say, выводящая сообщение в подходящем стиле и список персонажей

---
+++ 02_PushPip/cow_say.py
@@ -0,0 +1,13 @@
+import cowsay
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('message')
+parser.add_argument('-c', '--character', default='default')
+parser.add_argument('-l', '--list', action='store_false')
+args = parser.parse_args()
+
+if args.list:
+    print(cowsay.list_cows())
+else:
+    cowsay.cowsay(args.message, cow=args.character)
